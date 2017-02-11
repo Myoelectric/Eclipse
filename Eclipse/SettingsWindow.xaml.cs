a9dde53ae9e035b66e1c45751f1c9fb089636f3e
Cole Lanum <clanum@outlook.com>
2017-02-11 10:39:48 -0600

    Settings Window layout fix and rename.

--- Eclipse/SettingsWindow.xaml.cs
+++ Eclipse/SettingsWindow.xaml.cs
@@ -11,6 +11,13 @@ namespace Eclipse
 
             TextBoxMenuSize.Text = Properties.Settings.Default.MenuSize.ToString();
             TextBoxOpenDelay.Text = Properties.Settings.Default.OpenDelay.ToString();
+
+            ButtonSave.Click += ButtonSave_Click;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
